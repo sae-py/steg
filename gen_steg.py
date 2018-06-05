8a93dd62918446a0a57eb444c7adce816db30283
saepy <sae.94@outlook.jp>
2018-06-05 19:11:55 +0900

    Generalized

--- gen_steg.py
+++ gen_steg.py
@@ -6,13 +6,11 @@ fl = Image.open("埋め込みたい文字の画像")
 flag = fl.convert("RGB")
 
 
-for x in range(480):
-    for x in range(640):
+for x in range([x-size]):
+    for y in range([y-size]):
         r,g,b = image.getpixel((x, y))
 		# 下位1bitを操作
-        r = r&11111110 | int(flag.getpixel((x, y))[0] > 128)<<1
-        #r = r&11111101 | int(flag.getpixel((x, y))[0] > 128)<<1
-        #r = r&11111011 | int(flag.getpixel((x, y))[0] > 128)<<1
+        r = r&0b11111110 | int(flag.getpixel((x, y))[0] > 128)<<1
         image.putpixel((x, y), (r,g,b))
 
 image.save("完成画像")
